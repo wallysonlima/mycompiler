224d95dd46ee3158f955b4c07c8d1a3077787d47
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-08-12 17:00:12 -0300

    Created model

---
+++ myCompiler/src/model/Analise.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author wallysonlima
+ */
+public class Analise {
+    private String lexema, token, valor, linha, coluna;
+
+    public String getLexema() {
+        return lexema;
+    }
+
+    public void setLexema(String lexema) {
+        this.lexema = lexema;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getLinha() {
+        return linha;
+    }
+
+    public void setLinha(String linha) {
+        this.linha = linha;
+    }
+
+    public String getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(String coluna) {
+        this.coluna = coluna;
+    }
+}
