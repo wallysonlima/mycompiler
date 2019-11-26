03500f1c69afd8d22877f328fdf70eb2183a23c0
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-11-26 19:19:35 -0300

    Finished Stack class

--- myCompiler/src/model/Stack.java
+++ myCompiler/src/model/Stack.java
@@ -10,5 +10,44 @@ package model;
  * @author wallysonlima
  */
 public class Stack {
+    public Object[] stack;
+    public int position;
     
+    public Stack() {
+        this.position = -1;
+        this.stack = new Object[1000];
+    }
+    
+    public boolean isEmpty() {
+        if ( this.position == - 1)
+            return true;
+        
+        return false;
+    }
+    
+    public int size() {
+        if ( this.isEmpty() )
+            return 0;
+        
+        return this.position + 1;
+    }
+    
+    public Object getTop() {
+        if ( this.isEmpty() )
+            return null;
+        
+        return this.stack[this.position];
+    }
+    
+    public Object pop() {
+        if ( isEmpty() )
+            return null;
+        
+        return this.stack[this.position--];
+    }
+    
+    public void push(Object value) {
+        if ( this.position < this.stack.length - 1)
+            this.stack[++position] = value; 
+    }
 }
